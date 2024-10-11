ec5d0e94cb60f363fcbb3e7a886d8dff4ae42db3
Mateus Paulino <mateus.araujo08@eaportal.org>
2024-10-11 10:44:37 +0000

    commit da atividade pronta

---
+++ script.js
@@ -0,0 +1,12 @@
+function multiplicar() {
+    const numero1 = Number(document.querySelector("#n1").value)
+    const numero2 = Number(document.querySelector("#n2").value)
+    const numero3 = Number(document.querySelector("#n3").value)
+
+    const paragrafo = document.querySelector(".resultado")
+
+    paragrafo.innerHTML = `Resultado: ${numero1 * numero2 * numero3}`
+}
+
+const button = document.querySelector("button")
+button.addEventListener('click', multiplicar)
